4968d592735473e7ef98bb39662b5fd02df536ec
Ganesh Bhutekar <bhutekarganesh2003@gmail.com>
2026-07-21 23:41:09 +0530

    initial spring boot project setup

---
+++ src/main/java/com/cdac/onlineTiffinService/OnlineTiffinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.onlineTiffinService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTiffinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTiffinServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdac/onlineTiffinService/OnlineTiffinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.onlineTiffinService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTiffinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
